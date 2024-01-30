ecbbbb4099bbc3c2ba8543da7edcee9f6000563c
Your Name <you@example.com>
2024-01-30 17:28:53 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,151 @@
+'use strict'
+
+
+// const arr = [1,2,3,4,5]
+
+// arr.forEach(function (item,index,arr){
+//     console.log((`${index}:  ${item} has value ${arr}`));
+// })
+
+// arr.pop()
+// arr.push(6)
+
+
+// console.log(arr);
+
+
+// const arr = ['lazzizbek', 'lazizbe', 'usmon', 'salom ','laziz' ] 
+
+
+
+// arr.pop()
+// arr.pop()
+// arr.push('qalesilar')
+// arr.push('nimagap')
+
+
+// console.log(arr);
+
+// for(let i = 0; i < arr.length;i++){
+//     console.log(i);
+// }
+
+
+
+
+// for(let value of arr){
+//     console.log(value);
+// }-++
+
+
+
+
+
+
+
+
+
+
+// let movies = prompt('qaysi kino', 'kjhgf, lkjhgfd, lkiuyt, uyf, oiuyt');
+
+// const userMovies =  movies.split(', ')
+
+// // sort
+// userMovies.sort((a,b) => a- b   )
+// console.log(userMovies);
+// // joim
+
+// console.log(userMovies.join(`-m `));
+
+
+
+
+
+clonlash
+
+
+
+// let x = 10
+// let y = x
+
+// m = y + 1
+
+// console.log(m);
+
+
+// const numbers ={
+//     x: 10,
+//     y: 12,
+
+// }
+// const addObl ={
+//     z:125346
+
+// }
+
+
+
+// const newNumbers = Object.assign(addObl, numbers)
+
+
+// console.log(newNumbers);
+
+
+
+// const arr = [1,2,3,4,]
+
+// const newArr = arr.slice()
+
+// newArr[1] = 'lazizbek'
+
+
+// console.log(newArr);
+
+
+
+
+speator
+
+
+// const chelsea = ['palmaer', 'james']
+// const real = ['vini', 'kross']
+// const mixClub = [...chelsea, ...real]
+
+
+// console.log(mixClub);
+
+
+spr  
+
+
+let  arr = [1,2,3]
+
+function logger (x,z,y){
+    console.log(x + z + y);
+
+}
+
+logger(...arr)
+Object(...addObl)
+
+
+const numbers ={
+    x: 10,
+    y: 12,
+
+}
+const addObl ={
+    z:125346
+
+}
+
+
+
+const newNumbers = Object.assign(addObl, numbers)
+
+
+console.log(newNumbers);
+
+
+
+
